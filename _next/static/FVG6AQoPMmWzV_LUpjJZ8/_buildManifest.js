71c8bfab62b50ae31e54d14b8262c281e6cd0ccc
bluenex <tulakan.r@gmail.com>
2022-11-02 17:15:47 +0000

    Deploying to gh-pages from @ bluenex/simple-budget@eadf5f6c200a974f788e72f580cb1f5c0741457c 🚀

---
+++ _next/static/FVG6AQoPMmWzV_LUpjJZ8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/489-7e8e1f9c24a2330d.js","static/chunks/pages/index-4041bb6a02d533cb.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
