b7af7bd4a7d3d14a82cb8631661a802befe32a0b
bluenex <tulakan.r@gmail.com>
2022-11-02 21:43:25 +0700

    refactor: add visual for menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { FC, useCallback, useEffect, useState } from "react";
+import {
+  FiArrowDown,
+  FiArrowUp,
+  FiEdit,
+  FiRepeat,
+  FiTrash2,
+} from "react-icons/fi";
 import { twMerge } from "tailwind-merge";
 import { useLocalStorage } from "usehooks-ts";
 import { v4 as uuidv4 } from "uuid";
 import AddButton from "../src/components/AddButton";
 import AddingWindow from "../src/components/AddingWindow";
+import Button from "../src/components/Button";
 import Layout from "../src/components/Layout";
 
 export type ItemType = "income" | "expense";
@@ -31,20 +39,54 @@ const defaultData: DataSchema = {
   income: [],
 };
 
-const ItemRenderer: FC<{ itemData: RecordItem; itemType: ItemType }> = ({
-  itemData,
-  itemType,
-}) => {
+const ItemRenderer: FC<{
+  itemData: RecordItem;
+  itemType: ItemType;
+  onClick: () => void;
+  isMenuVisible: boolean;
+}> = ({ itemData, itemType, isMenuVisible, onClick }) => {
   return (
-    <div className="mb-0.5 flex w-full justify-between">
-      <p>{itemData.note}</p>
-      <p
+    <>
+      <div
         className={twMerge(
-          itemType === "expense" && "text-red-300",
-          itemType === "income" && "text-green-400",
+          "pointer-events-none h-0 w-full rounded-t bg-black bg-opacity-25 opacity-0 transition-all duration-300",
+          "flex justify-evenly text-lg",
+          isMenuVisible && "pointer-events-auto h-10 opacity-100",
         )}
-      >{`${itemType === "expense" ? "-" : "+"}${itemData.amount}`}</p>
-    </div>
+      >
+        <Button className="grid flex-1 place-content-center">
+          <FiArrowUp />
+        </Button>
+        <Button className="grid flex-1 place-content-center">
+          <FiArrowDown />
+        </Button>
+        <Button className="grid flex-1 place-content-center">
+          <FiRepeat />
+        </Button>
+        <Button className="grid flex-1 place-content-center">
+          <FiEdit />
+        </Button>
+        <Button className="grid flex-1 place-content-center">
+          <FiTrash2 />
+        </Button>
+      </div>
+
+      <div
+        className={twMerge(
+          "mb-0.5 flex w-full justify-between p-0.5 px-2 active:bg-black active:bg-opacity-10",
+          isMenuVisible && "bg-black bg-opacity-25",
+        )}
+        onClick={() => onClick()}
+      >
+        <p>{itemData.note}</p>
+        <p
+          className={twMerge(
+            itemType === "expense" && "text-red-300",
+            itemType === "income" && "text-green-400",
+          )}
+        >{`${itemType === "expense" ? "-" : "+"}${itemData.amount}`}</p>
+      </div>
+    </>
   );
 };
 
@@ -55,8 +97,8 @@ const TotalRenderer: FC<{ amount: number; itemType: ItemType }> = ({
   return (
     <div>
       <hr className="my-2" />
-      <div className="flex w-full justify-between">
-        <p className="font-bold">Sum:</p>
+      <div className="flex w-full justify-between px-2">
+        <p className="font-bold">Total:</p>
         <p
           className={twMerge(
             itemType === "expense" && "text-red-300",
@@ -91,6 +133,7 @@ const Home = () => {
 
   const [data, setData] = useState<DataSchema>(defaultData);
   const [addingType, setAddingType] = useState<ItemTypeWithHidden>("hidden");
+  const [menuVisibleId, setMenuVisibleId] = useState<string>();
 
   const dataUpdater = useCallback(
     (currentType: ItemType, amount: number, note: string) => {
@@ -128,7 +171,13 @@ const Home = () => {
               <h2 className="mb-2 text-lg font-bold">Expense</h2>
 
               {data.expense.map((ex) => (
-                <ItemRenderer itemType="expense" key={ex.id} itemData={ex} />
+                <ItemRenderer
+                  itemType="expense"
+                  key={ex.id}
+                  itemData={ex}
+                  isMenuVisible={menuVisibleId === ex.id}
+                  onClick={() => setMenuVisibleId(ex.id)}
+                />
               ))}
 
               <TotalRenderer itemType="expense" amount={getSum(data.expense)} />
@@ -140,7 +189,15 @@ const Home = () => {
               <h2 className="mb-2 text-lg font-bold">Income</h2>
 
               {data.income.map((inc) => (
-                <ItemRenderer itemType="income" key={inc.id} itemData={inc} />
+                <ItemRenderer
+                  itemType="income"
+                  key={inc.id}
+                  itemData={inc}
+                  isMenuVisible={menuVisibleId === inc.id}
+                  onClick={() =>
+                    setMenuVisibleId((p) => (inc.id !== p ? inc.id : undefined))
+                  }
+                />
               ))}
 
               <TotalRenderer itemType="income" amount={getSum(data.income)} />
