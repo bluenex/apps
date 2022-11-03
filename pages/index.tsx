97f3d07050c5d94beeb1114f56f0415756ca3617
bluenex <tulakan.r@gmail.com>
2022-11-03 17:00:45 +0700

    refactor: reduce pb

--- pages/index.tsx
+++ pages/index.tsx
@@ -417,7 +417,7 @@ const Home = () => {
       <main>
         {data.expense.length === 0 && data.income.length === 0 && "No record!"}
 
-        <section id="render-data" className="pb-20">
+        <section id="render-data" className="pb-14">
           {data?.diff !== undefined && (
             <div id="diff" className="mb-6">
               <h2 className="mb-2 text-lg font-bold">Diff</h2>
